4ce9a0dd738abe8142b723c43096116899a73e6b
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-07-25 21:36:40 +0300

    Add variables.py with basic variable operations and print statements

---
+++ variables.py
@@ -0,0 +1,18 @@
+# maasAli = 5000
+# maasVeli = 6000
+
+# vergi = 0.27
+
+# print("Ali'nin maaşı:", maasAli - (maasAli * vergi))
+# print("Veli'nin maaşı:", maasVeli - (maasVeli * vergi))
+
+firstName = "Rıdvan"
+lastName = "Aslan"
+print(firstName + " " + lastName)
+
+x, y, name, isTrue = 5, 10, "Rıdvan", True
+
+print("x:", x)
+print("y:", y)
+print("Name:", name)
+print("Is True:", isTrue)
