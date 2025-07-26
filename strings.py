eace584c3dbb7654b1777c746a111e43b3122138
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-07-26 10:17:46 +0300

    Add examples of string slicing and indexing

---
+++ strings.py
@@ -0,0 +1,17 @@
+name = "Rıdvan"
+surname = "Aslan"
+age = 22
+
+greeting = "My name is " + name + " " + surname + " and \nI am " + str(age) + " years old."
+length = len(greeting)
+
+# print(greeting)
+# print(greeting[0])
+# print(greeting[3])
+# print(greeting[length - 1])
+# print(greeting[-1])
+
+# print(greeting[0:4])  
+# print(greeting[0:10:2]) 
+# print(greeting[10:])  
+print(greeting[:10])
