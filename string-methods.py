42fe8113c0170276fcb547f41df204ebf467eb73
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-07-26 12:35:35 +0300

    apply different string methods

---
+++ string-methods.py
@@ -0,0 +1,27 @@
+message = "Hello There. My name is Rıdvan Aslan. I am a Software Developer."
+
+# message = message.upper()
+# message = message.lower()
+# message = message.title()
+# message = message.capitalize()
+
+# message = message.strip()
+# message = message.split()
+# message = message.split(".")
+# message = " ".join(message.split())
+# message = "---".join(message.split())
+
+# print(message[2])
+
+# index = message.find("Rıdvan")
+# print(f"Index of 'Rıdvan': {index}")
+
+# isFound = message.startswith("Hello")
+# print(f"Does the message start with 'Hello'? {isFound}")
+# isFound = message.endswith("Developer.")
+# print(f"Does the message end with 'Developer.'? {isFound}")
+
+# message = message.replace("Developer", "Engineer").replace("There", "World")
+
+message = message.center(100, "*")
+print(message)
