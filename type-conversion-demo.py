2b0f0f808648fa2afa6a1432fcd156c5e0f5ed66
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-07-26 09:47:14 +0300

    Add type conversion practice examples

---
+++ type-conversion-demo.py
@@ -0,0 +1,10 @@
+#Calculating circle area and circle circumference
+
+pi = 3.14
+r = input("Enter the radius of the circle: ")
+
+circle_area = pi * float(r) ** 2
+circle_circumference = 2 * pi * float(r)
+
+print("Circle Area:", circle_area)
+print("Circle Circumference:", circle_circumference)
