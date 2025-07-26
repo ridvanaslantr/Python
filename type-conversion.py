22579d43929d73578d50629eefed8c0dcf538d0f
Rıdvan Aslan <ridvan_aslan.tr@icloud.com>
2025-07-26 09:36:32 +0300

    Implement basic type casting operations in Python

---
+++ type-conversion.py
@@ -0,0 +1,31 @@
+# x = input("Enter first number: ")
+# y = input("Enter second number: ")
+
+# print(type(x), type(y))
+
+# total = int(x) + int(y)
+
+# print("The total is:", total)
+
+x = 5 
+y = 2.5
+name = "Rıdvan"
+isOnline = True
+
+# Type conversion example
+
+# int to float
+# x_float = float(x)
+# print("x as float:", x_float, type(x_float))
+
+# float to int
+# y_int = int(y)  
+# print("y as int:", y_int, type(y_int))
+
+# bool to str
+# isOnline_str = str(isOnline)
+# print("isOnline as string:", isOnline_str, type(isOnline_str))
+
+#bool to int
+isOnline_int = int(isOnline)
+print("isOnline as int:", isOnline_int, type(isOnline_int))
